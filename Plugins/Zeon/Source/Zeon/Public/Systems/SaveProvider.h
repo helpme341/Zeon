272366917b4228a3401bb27d82334b1a837778e6
Ilia <137097948+helpme341@users.noreply.github.com>
2026-03-18 21:24:16 -0400

    SaveProvider Change

---
+++ Plugins/Zeon/Source/Zeon/Public/Systems/SaveProvider.h
@@ -0,0 +1,32 @@
+﻿
+#pragma once
+
+#include "CoreMinimal.h"
+#include "StructUtils/InstancedStruct.h"
+#include "SaveProvider.generated.h"
+
+UINTERFACE(BlueprintType)
+class ZEON_API USaveProvider : public UInterface
+{
+	GENERATED_BODY()
+};
+
+class ZEON_API ISaveProvider
+{
+	GENERATED_BODY()
+
+public:
+	
+	UFUNCTION(BlueprintNativeEvent, BlueprintCallable, Category="SaveProvider")
+	virtual FName GetSaveDomain() const = 0;
+	
+	UFUNCTION(BlueprintNativeEvent, BlueprintCallable, Category="SaveProvider")
+	virtual FGuid GetPersistentID() const = 0;
+
+	
+	UFUNCTION(BlueprintNativeEvent, BlueprintCallable, Category="SaveProvider")
+	virtual bool CaptureSaveData(FInstancedStruct& OutData) const = 0;
+	
+	UFUNCTION(BlueprintNativeEvent, BlueprintCallable, Category="SaveProvider")
+	virtual bool RestoreSaveData(const FInstancedStruct& InData) = 0;
+};
